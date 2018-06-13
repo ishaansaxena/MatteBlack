00a63e7e1f57b5952dc193da78c64250fd88cfd5
Ishaan <isaxena@purdue.edu>
2018-06-13 17:43:47 +0530

    Add stocks views

--- server/stocks/views.py
+++ server/stocks/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 def stock_index_view(request):
     context = {}
-    render(request, 'stocks/index.html', context)
+    return render(request, 'stocks/index.html', context)
 
 def stock_default_view(request, symbol):
     context = {
         'symbol': symbol,
     }
-    render(request, 'stocks/stock.html', context)
+    return render(request, 'stocks/stock.html', context)
