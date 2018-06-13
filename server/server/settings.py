2dbf3f6da4bb371c38306a32147e2aeb4307c32f
Ishaan <isaxena@purdue.edu>
2018-06-13 09:59:01 +0530

    Add base and index templates

--- server/server/settings.py
+++ server/server/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIRS = [os.path.join(BASE_DIR, 'templates'), ]
 
 
 # Get AlphaVantage API Key from env
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': TEMPLATES_DIRS,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
