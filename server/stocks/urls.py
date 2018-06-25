b1fce10fff57e2b446f8d6ada9944a34e43e4b62
Ishaan <isaxena@purdue.edu>
2018-06-25 11:51:03 +0530

    Modularize stock viewss

--- server/stocks/urls.py
+++ server/stocks/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'stocks'
 
 urlpatterns = [
-    url(r'^(?P<symbol>[-\w]+)/$', views.stock_default_view, name='stock'),
+    url(r'^(?P<symbol>[-\w]+)/$', views.stock_timeseries_view, name='stock'),
     url('', views.stock_index_view, name='index'),
 ]
--- server/stocks/views.py
+++ server/stocks/views.py
@@ -7,50 +7,68 @@ import pandas
 from alpha_vantage.timeseries import TimeSeries
 from django.core.serializers.json import DjangoJSONEncoder
 
+# Get API Key
 KEY = settings.AV_API_KEY
 
-def printIfDebug(str):
-    if settings.DEBUG:
-        print(str)
 
-def stock_index_view(request):
-    context = {}
-    return render(request, 'stocks/index.html', context)
+# Initialize timeseries
+TS = TimeSeries(key=KEY, output_format='pandas')
 
-def stock_default_view(request, symbol):
-    # Convert symbol to upper case
-    symbol = symbol.upper()
 
-    # Initialize timeseries
-    ts = TimeSeries(key=KEY, output_format='pandas')
+# Function maps
+TS_MAP = {
+    "intraday": TS.get_intraday,
+}
 
-    # Get data and metadata
-    try:
-        data, meta_data = ts.get_intraday(symbol=symbol,interval='1min')
-    except:
-        raise Http404("%s is not a valid symbol." % symbol)
+ARGS_MAP = {
+    "intraday": {"interval": "1min"}
+}
 
-    # Process data into json format
-    data_dict = {}
-    data_dict["dates"] = data.index.values.tolist()
-    for column in data:
-        data_dict[column] = data[column].values.tolist()
 
-    # Get organization details
+# Generic helper methods
+def get_stock_info(symbol):
     try:
         info = settings.TICKER_DATA[symbol]
     except:
         info = {"Name": "", "Sector": "", "Industry": ""}
         print("Details not found for %s" % symbol)
+    return info
+
+def get_json_data(data):
+    data_dict = {}
+    data_dict["dates"] = data.index.values.tolist()
+    for column in data:
+        data_dict[column] = data[column].values.tolist()
+    return data_dict
+
+
+# View methods
+def stock_index_view(request):
+    context = {}
+    return render(request, 'stocks/index.html', context)
+
+def stock_timeseries_view(request, symbol):
+    symbol = symbol.upper()
+
+    # Get data and metadata
+    format = "intraday"
+    try:
+        kwargs = {"symbol": symbol}
+        kwargs.update(ARGS_MAP[format])
+        data, meta_data = TS_MAP[format](**kwargs)
+    except:
+        raise Http404("%s is not a valid symbol." % symbol)
+
+    # Process data into json format and get organization details
+    data_dict   = get_json_data(data)
+    info        = get_stock_info(symbol)
 
     # Create context
     context = {
         'symbol': symbol,
         'data': json.dumps(data_dict, cls=DjangoJSONEncoder),
-        # 'data': data,
-        'info': info,
-        'meta': meta_data
     }
     context.update(info)
     context.update(meta_data)
+    print(meta_data)
     return render(request, 'stocks/stock.html', context)
