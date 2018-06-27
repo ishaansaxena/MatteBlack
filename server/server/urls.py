054f0e3eebdf7cf8da80c4bdda89f459affffd91
Ishaan <isaxena@purdue.edu>
2018-06-27 10:31:38 +0530

    Change templating inheritance structure

--- server/server/urls.py
+++ server/server/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html"), name="home"),
+    path('', TemplateView.as_view(template_name="global/index.html"), name="home"),
     path('s/', include('stocks.urls', namespace='s')),
     path('u/', include('investor.urls', namespace='u')),
 ]
