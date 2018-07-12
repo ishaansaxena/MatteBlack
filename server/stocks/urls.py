b08bb0b0851f4b5a206c6055e6c659c8c479b618
Ishaan <isaxena@purdue.edu>
2018-07-12 17:28:56 +0530

    Add URI todos

--- server/stocks/urls.py
+++ server/stocks/urls.py
@@ -11,3 +11,9 @@ urlpatterns = [
     url(r'^(?P<symbol>[-\w]+)/$', views.stock_timeseries_default, name='stock'),
     url('', views.stock_index_view, name='index'),
 ]
+
+# TODO: In the future, we need to add support for different exchanges. Eg: "NSE:BPL"
+# url(r'^(?P<symbol>[-\w]+[:]*[-\w]+)/track/$', views.track_stock_view, name='track'),
+# url(r'^(?P<symbol>[-\w]+[:]*[-\w]+)/untrack/$', views.untrack_stock_view, name='untrack'),
+# url(r'^(?P<symbol>[-\w]+[:]*[-\w]+)/t/(?P<format>[-\w]+)/$', views.stock_timeseries_view, name='timeseries'),
+# url(r'^(?P<symbol>[-\w]+[:]*[-\w]+)/$', views.stock_timeseries_default, name='stock'),
