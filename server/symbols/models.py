25c2fc251ad7b73942f79bf80d315fad191e9037
Ishaan <isaxena@purdue.edu>
2018-07-12 14:07:17 +0530

    Add symbol track/untrack views

--- server/symbols/models.py
+++ server/symbols/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+from investor.models import Investor
+
 class Symbol(models.Model):
-    type = models.TextField()
-    symbol = models.TextField()
+    type = models.CharField(max_length=16)
+    symbol = models.CharField(max_length=16)
     slug = models.SlugField()
+    trackers = models.ManyToManyField(Investor, symmetrical=False, blank=True, default=None)
+
+    def __str__(self):
+        return self.type + ":" + self.symbol
--- server/symbols/views.py
+++ server/symbols/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from symbols.models import Symbol
+from investor.models import Investor
+
+## Maps
+SLUG_MAP = {
+    'stock': '/s/{}/',
+    'crypto': '/c/{}/',
+    'forex': '/f/{}/',
+}
+
+
+## Helper methods
+
+# Get Symbol information
+def get_symbol_data(symbol):
+    type = symbol.split(':')[0]
+    symbol = symbol.split(':')[1]
+    slug = SLUG_MAP[type].format(symbol)
+    return type, symbol, slug
+
+
+## View methods
+
+# Track symbol view
+def track_symbol_view(request, symbol):
+    type, symbol, slug = get_symbol_data(symbol)
+    obj, created = Symbol.objects.get_or_create(
+        type=type, symbol=symbol, slug=slug
+    )
+    tracker = Investor.objects.get(user=request.user)
+    obj.trackers.add(tracker)
+    data = {
+        'symbol':   symbol,
+    }
+    return JsonResponse(data)
+
+# Track symbol view
+def untrack_symbol_view(request, symbol):
+    type, symbol, slug = get_symbol_data(symbol)
+    obj = Symbol.objects.get(
+        type=type, symbol=symbol, slug=slug
+    )
+    tracker = Investor.objects.get(user=request.user)
+    obj.trackers.remove(tracker)
+    data = {
+        'symbol':   symbol,
+    }
+    return JsonResponse(data)
