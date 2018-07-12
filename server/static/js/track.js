ccfc39331fc03584f766463d4b88bb1fdeed0b17
Ishaan <isaxena@purdue.edu>
2018-07-12 15:10:38 +0530

    Tracking with AJAX requests

--- server/static/js/track.js
+++ server/static/js/track.js
@@ -3,12 +3,16 @@ $(document).ready(function() {
         console.log(trackURL);
         $.get(trackURL, function(data) {
             console.log(data);
+            $('#track-outer').addClass('hide');
+            $('#untrack-outer').removeClass('hide');
         });
     });
     $('#untrack').click(function() {
-        console.log(trackURL);
+        console.log(untrackURL);
         $.get(untrackURL, function(data) {
             console.log(data);
+            $('#untrack-outer').addClass('hide');
+            $('#track-outer').removeClass('hide');
         });
     });
 });
