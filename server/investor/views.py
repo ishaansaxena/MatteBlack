140a398553f4cfd96ad34ec2e33c0335119055e4
Ishaan <isaxena@purdue.edu>
2018-07-12 12:53:14 +0530

    Change symbol track view

--- server/investor/views.py
+++ server/investor/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from symbols.models import Symbol
+
+## Maps
+SLUG_MAP = {
+    'stock': '/s/{}/',
+    'crypto': '/c/{}/',
+    'forex': '/f/{}/',
+}
+
+
+## Helper methods
+
+# Get Symbol information
+def get_symbol_data(symbol):
+    type = symbol.split(':')[0]
+    symbol = symbol.split(':')[1]
+    slug = SLUG_MAP[type].format(symbol)
+    return type, symbol, slug
+
+
 ## View methods
 
 # Registration
@@ -10,8 +30,13 @@ def investor_register_view(request):
 
 # Track symbol view
 def track_symbol_view(request, symbol):
+    type, symbol, slug = get_symbol_data(symbol)
+    obj, created = Symbol.objects.get_or_create(
+        type=type, symbol=symbol, slug=slug
+    )
     data = {
         'symbol':   symbol,
+        'created':  created,
     }
-    print(data)
+    print(created)
     return JsonResponse(data)
